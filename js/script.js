8eab870d854144cbd054892a9d068c3526be1928
JuanFidelDelgado <delgadojuanfidel@gmail.com>
2023-06-13 17:58:11 -0500

    Commit inicial del proyecto

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Función para cambiar de pestaña al hacer clic
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    // Ocultar todo el contenido de las pestañas
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Desactivar todas las pestañas
+    tablinks = document.getElementsByClassName("tab");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Mostrar el contenido de la pestaña seleccionada y activar la pestaña correspondiente
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Establecer la pestaña activa inicialmente
+document.getElementById("defaultOpen").click();
